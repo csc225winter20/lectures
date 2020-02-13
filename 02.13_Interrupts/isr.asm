37c5947028444a0841acbc7b24a19539089f010b
Christopher Siu <christopher@siu.sh>
2020-02-13 11:03:56 -0800

    Base code for interrupts.

---
+++ 02.13_Interrupts/isr.asm
@@ -0,0 +1,21 @@
+; NOTE: Must also set 0xFE00 to 0x4000 and 0x0180 to 0x0300.
+
+            .ORIG x300
+KB_ISR      ST  R0, SAVER0
+            ST  R1, SAVER1
+
+            LD  R0, SAVE_ADDR
+            LDI R1, KBDR        ; Get typed character.
+            STR R1, R0, #0      ; Save in user-space.
+            ADD R0, R0, #1      ; Increment save addr.
+            ST  R0, SAVE_ADDR
+
+            LD  R1, SAVER1
+            LD  R0, SAVER0
+            RTI
+
+SAVER0      .FILL x0000
+SAVER1      .FILL x0000
+SAVE_ADDR   .FILL x4000
+KBDR        .FILL xFE02
+            .END
