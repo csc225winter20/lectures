6684b21e2c3c453e9e99cfa17ebc6ab6f560630d
Christopher Siu <christopher@siu.sh>
2020-02-13 18:02:01 -0800

    Polling for GETC/OUT.

--- 02.13_InputOutput/getc.asm
+++ 02.13_InputOutput/getc.asm
@@ -1,7 +1,8 @@
 OS_KBSR         .FILL xFE00
 OS_KBDR         .FILL xFE02
 
-TRAP_GETC       LDI R0, OS_KBSR
-                BRzp TRAP_GETC
-                LDI R0, OS_KBDR
-                RTI
+; These instructions start at memory location 0x021B:
+TRAP_GETC       LDI R0, OS_KBSR     ; Load the KBSR into R0.
+                BRzp TRAP_GETC      ; ...poll until the KBSR is negative.
+                LDI R0, OS_KBDR     ; Load the char in the KBDR.
+                RTI                 ; Return.
--- 02.13_InputOutput/out.asm
+++ 02.13_InputOutput/out.asm
@@ -2,9 +2,10 @@ OS_DSR          .FILL xFE04
 OS_DDR          .FILL xFE06
 TOUT_R1         .BLKW #1
 
-TRAP_OUT        ST  R1, TOUT_R1
-TRAP_OUT_WAIT   LDI R1, OS_DSR
-                BRzp TRAP_OUT_WAIT
-                STI R0, OS_DDR
-                LD  R1, TOUT_R1
-                RTI
+; NOTE: R0 contains the ASCII value of the character to be displayed.
+TRAP_OUT        ST  R1, TOUT_R1     ; Save R1.
+TRAP_OUT_WAIT   LDI R1, OS_DSR      ; Load the DSR into R1.
+                BRzp TRAP_OUT_WAIT  ; ...poll until the DSR is negative.
+                STI R0, OS_DDR      ; Store the char in the DDR.
+                LD  R1, TOUT_R1     ; Restore R1.
+                RTI                 ; Return.
