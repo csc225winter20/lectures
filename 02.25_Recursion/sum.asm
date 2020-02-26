377280334408ce02fc98c8e2552b8dbba8842afb
Christopher Siu <christopher@siu.sh>
2020-02-25 19:38:43 -0800

    Recursive LC-3 functions

--- 02.25_Recursion/sum.asm
+++ 02.25_Recursion/sum.asm
@@ -32,11 +32,43 @@ NEWLINE .FILL x0A
 
 ; int sum(int)
 SUMFN   ; CallEE setup:
+        ADD R6, R6, #-1     ; Push space for the return value.
+        ADD R6, R6, #-1     ; Push the return address.
+        STR R7, R6, #0
+        ADD R6, R6, #-1     ; Push the dynamic link.
+        STR R5, R6, #0
+        ADD R5, R6, #-1     ; Set the frame pointer.
+                            ; NOTE: There are no locals.
 
+        LDR R0, R5, #4      ; Load "n" into R0.
+        BRp SUMELSE         ; If less than or equal to zero...
+        ; NOTE: Base case:
+        AND R0, R0, #0      ; ...set return value to 0...
+        STR R0, R5, #3
+        BRnzp SUMRET        ; Else...
+
+SUMELSE ; NOTE: Recursive case:
         ; CallER setup:
+        ADD R0, R0, #-1     ; ...compute "n - 1"...
+        ADD R6, R6, #-1     ; ...push "n - 1"...
+        STR R0, R6, #0
+        JSR SUMFN           ; ...recurse on "sum(n - 1)"...
 
         ; CallER teardown:
+        LDR R1, R6, #0      ; ...pop "sum(n - 1)" into R1...
+        ADD R6, R6, #1
+        ADD R6, R6, #1      ; ...pop arguments...
+
+        LDR R0, R5, #4      ; ...load "n" into R0...
+        ADD R0, R0, R1      ; ...compute "sum(n - 1) + n"...
+        STR R0, R5, #3      ; ...set return value to "sum(n - 1) + n"...
 
         ; CallEE teardown:
+SUMRET                      ; NOTE: There were no locals.
+        LDR R5, R6, #0      ; Pop the dynamic link.
+        ADD R6, R6, #1
+        LDR R7, R6, #0      ; Pop the return address.
+        ADD R6, R6, #1
+        RET                 ; Return.
 
         .END
--- 02.25_Recursion/sum.c
+++ 02.25_Recursion/sum.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+int sum(int);
+
 int main(void) {
     printf("3 + 2 + 1 + 0 = %d\n", sum(3));
 
     return 0;
+}
+
+int sum(int n) {
+    if (n <= 0) {
+        return 0;
+    }
+    else {
+        return sum(n - 1) + n;
+    }
 }
